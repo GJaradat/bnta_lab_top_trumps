f11018dba8cb16ccd1106101bf25180b2d39d2b9
Ralphael Obadiaru <ralph.obadiaru@gmail.com>
2024-02-26 16:04:18 +0000

    Started implementing service layer

--- src/main/java/com/example/top_trumps_start_code/controllers/TopTrumpsController.java
+++ src/main/java/com/example/top_trumps_start_code/controllers/TopTrumpsController.java
@@ -2,12 +2,11 @@ package com.example.top_trumps_start_code.controllers;
 
 
 import com.example.top_trumps_start_code.models.*;
+import com.example.top_trumps_start_code.services.TopTrumpsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
@@ -15,11 +14,22 @@ import java.util.ArrayList;
 @RequestMapping(value = "/toptrumps")
 public class TopTrumpsController {
 
-//    private ArrayList<Card> cards;
+    private String round;
+
+//    @Autowired
+    TopTrumpsService service;
     @PostMapping
     public ResponseEntity<Reply> newRound(@RequestBody ArrayList<Card> cards){
-        Reply reply = new Reply("");
+        Reply reply = new Reply("Round started. Played hand");
+
         return new ResponseEntity<Reply>(reply, HttpStatus.CREATED);
+    }
+
+    @PatchMapping
+    public ResponseEntity<Reply> checkWinner(ArrayList<Card> cards) {
+        Reply reply = new Reply(service.checkWinner(cards));
+
+        return new ResponseEntity<Reply>(reply, HttpStatus.OK);
     }
 
 
--- src/main/java/com/example/top_trumps_start_code/services/TopTrumpsService.java
+++ src/main/java/com/example/top_trumps_start_code/services/TopTrumpsService.java
@@ -1,4 +1,24 @@
 package com.example.top_trumps_start_code.services;
 
+import com.example.top_trumps_start_code.models.Card;
+
+import java.util.ArrayList;
+
 public class TopTrumpsService {
+
+
+    public String checkWinner(ArrayList<Card> cards) {
+        if (cards.get(0).getCardValue() > cards.get(1).getCardValue()) {
+            return cards.get(0).getRank() + " of " + cards.get(0).getSuit() + " wins!";
+        } else if (cards.get(0).getCardValue() == cards.get(1).getCardValue()) {
+            return "This is a draw.";
+        }
+        return cards.get(1).getRank() + " of " + cards.get(1).getSuit() + " wins!";
+    }
+
+    public TopTrumpsService() {
+
+    }
+
 }
+
