87bf4d9835c52980d8a8da8585854a3fadb8b701
Gabriel Dzharadat <dzh.gabriel@gmail.com>
2024-02-26 15:05:53 +0000

    Added Reply class and Controller

---
+++ src/main/java/com/example/top_trumps_start_code/controllers/TopTrumpsController.java
@@ -0,0 +1,25 @@
+package com.example.top_trumps_start_code.controllers;
+
+
+import com.example.top_trumps_start_code.models.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping(value = "/toptrumps")
+public class TopTrumpsController {
+
+//    private ArrayList<Card> cards;
+    @PostMapping
+    public ResponseEntity<Reply> newRound(@RequestBody ArrayList<Card> cards){
+        Reply reply = new Reply("");
+        return new ResponseEntity<Reply>(reply, HttpStatus.CREATED);
+    }
+
+}
